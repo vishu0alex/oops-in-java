c1f26a991d4f58bb400c5088fabaa4e2f2dd917d
vishu0alex <vikas8146033487@gmail.com>
2025-05-24 17:37:19 +0530

    Update copy constructor.java

--- copy constructor.java
+++ copy constructor.java
@@ -1,20 +1,31 @@
- class product{
+class Product {
     int x;
     int y;
-    public void  pro(){
-           System.out.println("pro ="+(x*y));
+
+    // Regular constructor
+    Product(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
-    product(int x,int y){
-        System.out.println(x*y);
-        this.x=x;
-        this.y=y;
+
+    // Copy constructor
+    Product(Product other) {
+        this.x = other.x;
+        this.y = other.y;
     }
-   
+
+    public void pro() {
+        System.out.println("pro = " + (x * y));
+    }
 }
-public class Main{
-    public static void main(String[]args){
-        product obj=new product(4,6);
-        product obj2=obj;
-        obj2.pro();
-      }
+
+public class Main {
+    public static void main(String[] args) {
+        Product obj1 = new Product(5, 6);
+        Product obj2 = new Product(obj1);  // ✅ Separate object with same values
+
+        obj2.x = 10;
+        obj1.pro();  // Unaffected: pro = 30
+        obj2.pro();  // Changed: pro = 60
+    }
 }
